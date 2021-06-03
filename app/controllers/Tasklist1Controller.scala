edfdc785ebb456e4587e021392f2f403f1450187
consigliere19 <bitanu.ch@gmail.com>
2021-06-04 03:29:57 +0530

    Login1

--- app/controllers/Tasklist1Controller.scala
+++ app/controllers/Tasklist1Controller.scala
@@ -7,6 +7,27 @@ import play.api.mvc._
 @Singleton
 class Tasklist1Controller  @Inject()(val controllerComponents: ControllerComponents) extends BaseController{
     
+    def login = Action {
+        Ok(views.html.login1());
+    }
+
+    def validate(username: String, password: String) = Action {
+        Ok(s"$username logged in with $password");
+    }
+
+    def validatePost = Action { (request) =>
+        val postVals = request.body.asFormUrlEncoded;
+        postVals.map(args => {
+            val username = args("username").head;
+            val password = args("password").head;
+            Ok(s"$username logged in with $password");
+        }
+        ).getOrElse(
+            Ok("Oops!")
+        )
+    }
+
+
     def tasklist = Action {
         val tasks = List("Play Football", "Watch Football", "Sleep");
         Ok(views.html.tasklist1(tasks));
