b7811198a55a1158105123087006f601567e705c
consigliere19 <bitanu.ch@gmail.com>
2021-06-04 20:03:36 +0530

    Add and remove items

--- app/controllers/Tasklist1Controller.scala
+++ app/controllers/Tasklist1Controller.scala
@@ -8,33 +8,85 @@ import models.TasklistInMemoryModel
 @Singleton
 class Tasklist1Controller  @Inject()(val controllerComponents: ControllerComponents) extends BaseController{
     
-    def login = Action {
+    def login = Action { implicit request =>
         Ok(views.html.login1());
+    }
+
+    def logout = Action {
+        Redirect(routes.Tasklist1Controller.login()).withNewSession;
     }
 
     def validate(username: String, password: String) = Action {
         Ok(s"$username logged in with $password");
     }
 
-    def validatePost = Action { (request) =>
+    def validatePost = Action { implicit request =>
         val postVals = request.body.asFormUrlEncoded;
         postVals.map { args => 
             val username = args("username").head;
             val password = args("password").head;
             if (TasklistInMemoryModel.validateUser(username, password))
-                Redirect(routes.Tasklist1Controller.tasklist());
+                Redirect(routes.Tasklist1Controller.tasklist()).withSession("username" -> username);
             else
-                Redirect(routes.Tasklist1Controller.login());
+                Redirect(routes.Tasklist1Controller.login()).flashing("error" -> "Invalid username or password.");
+        }.getOrElse(
+            Redirect(routes.Tasklist1Controller.login())
+        )
+    }
+
+    def createUser = Action { implicit request =>
+        val postVals = request.body.asFormUrlEncoded;
+        postVals.map { args => 
+            val username = args("username").head;
+            val password = args("password").head;
+            if (TasklistInMemoryModel.createUser(username, password))
+                Redirect(routes.Tasklist1Controller.tasklist()).withSession("username" -> username);
+            else
+                Redirect(routes.Tasklist1Controller.login()).flashing("error" -> "Failed to create new user.");
         }.getOrElse(
             Redirect(routes.Tasklist1Controller.login())
         )
     }
 
 
-    def tasklist = Action {
-        val username = "Kai";
-        val tasks = TasklistInMemoryModel.getTasks(username);
-        Ok(views.html.tasklist1(tasks));
+    def tasklist = Action { implicit request => 
+        val usernameOption = request.session.get("username");
+        usernameOption.map { username => 
+            val tasks = TasklistInMemoryModel.getTasks(username);
+            Ok(views.html.tasklist1(tasks));
+        }.getOrElse(Redirect(routes.Tasklist1Controller.login()));        
+    }
+
+    def addTask = Action { implicit request =>
+        val usernameOption = request.session.get("username");
+        usernameOption.map { username =>
+            val postVals = request.body.asFormUrlEncoded;
+            postVals.map { args => 
+                val task = args("newTask").head;
+                TasklistInMemoryModel.addTask(username, task);
+                Redirect(routes.Tasklist1Controller.tasklist())
+            }.getOrElse(
+                Redirect(routes.Tasklist1Controller.tasklist())
+            )
+        }.getOrElse(
+            Redirect(routes.Tasklist1Controller.login())
+        )
+    }
+
+    def deleteTask = Action { implicit request =>
+        val usernameOption = request.session.get("username");
+        usernameOption.map { username =>
+            val postVals = request.body.asFormUrlEncoded;
+            postVals.map { args => 
+                val index = args("index").head.toInt;
+                TasklistInMemoryModel.removeTask(username, index);
+                Redirect(routes.Tasklist1Controller.tasklist())
+            }.getOrElse(
+                Redirect(routes.Tasklist1Controller.tasklist())
+            )
+        }.getOrElse(
+            Redirect(routes.Tasklist1Controller.login())
+        )
     }
 
 }
--- app/models/TasklistInMemoryModel.scala
+++ app/models/TasklistInMemoryModel.scala
@@ -12,18 +12,26 @@ object TasklistInMemoryModel {
     }
 
     def createUser(username: String, password: String): Boolean = {
-        ???
+        if (users.contains(username)) false;
+        else{
+            users(username) = password;
+            true;
+        }
     }
 
     def getTasks(username: String): Seq[String] = {
         tasks.get(username).getOrElse(Nil);
     }
 
-    def addTask(username: String): Unit = {
-        ???
+    def addTask(username: String, task: String): Unit = {
+        tasks(username) = task :: tasks.get(username).getOrElse(Nil);
     }
 
     def removeTask(username: String, index: Int): Boolean = {
-        ???
+        if (index < 0 || tasks.get(username).isEmpty || index >= tasks(username).length) false;
+        else {
+            tasks(username) = tasks(username).patch(index, Nil, 1);
+            true;
+        }
     }
 }
